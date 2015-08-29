99b52f6ee4f7297ce824a9123f09c84129163769
Duy Nguyen <cgduy.nguyen@gmail.com>
2015-08-29 23:48:26 +0700

    Thêm file (tội ngu)

---
+++ CS427_FinalProject/CS427_FinalProject/NewGameViewHandler.cs
@@ -0,0 +1,29 @@
+﻿using CS427_FinalProject.Buttons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS427_FinalProject
+{
+    class NewGameViewHandler : MenuViewHandler
+    {
+        public NewGameViewHandler()
+        {
+            //this.buttons.Add(new Accept(555, 525));
+            //this.buttons.Add(new Menu(369, 542));
+            //this.buttons.Add(new Exit(777, 542));
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime, object param)
+        {
+            base.Draw(gameTime, param);
+            
+        }
+    }
+}
