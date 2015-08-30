43ba63704821c6b591683b3e7b4624681cb756cd
Minh <nkbminh@apcs.vn>
2015-08-30 13:43:56 +0700

    Cập nhật setting

--- CS427_FinalProject/CS427_FinalProject/SettingView.cs
+++ CS427_FinalProject/CS427_FinalProject/SettingView.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using CS427_FinalProject.Buttons;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -9,20 +10,39 @@ namespace CS427_FinalProject
 {
     class SettingView : MenuViewHandler
     {
+        Minus musicDown, soundDown;
+        Plus musicUp, soundUp;
+        
+
+        public int SoundLevel
+        {
+            get { return Global.gSound; }
+            set { Global.gSound = value; }
+        }
+
+        public int MusicLevel
+        {
+            get { return Global.gMusic; }
+            set { Global.gMusic = value; }
+        }
         public SettingView()
         {
-                       
+            musicDown = new Minus(985, 227);
+            musicUp = new Plus(1085, 227);  
+
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
+            musicDown.Update(gameTime);
+            musicUp.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime, object param)
         {
             base.Draw(gameTime, param);
-            Vector2 length = Global.gDefaultMediumFont.MeasureString("Audio");            
+            Vector2 length = Global.gDefaultMediumFont.MeasureString("Sound");            
             SpriteBatch spriteBatch = param as SpriteBatch;
             spriteBatch.DrawString(Global.gDefaultLargeFont, "How to play", new Vector2(102.5f, 127), Color.FromNonPremultiplied(206, 235, 12, 255), 0f, Vector2.Zero, 1f, SpriteEffects.None, 1);
             spriteBatch.DrawString(Global.gDefaultMediumFont, "Cat", new Vector2(123.5f, 227), Color.FromNonPremultiplied(206, 235, 12, 255), 0f, Vector2.Zero, 1f, SpriteEffects.None, 1);
@@ -38,7 +58,10 @@ namespace CS427_FinalProject
             spriteBatch.DrawString(Global.gDefaultMediumFont, "Down: Down", new Vector2(351.5f, 527), Color.FromNonPremultiplied(206, 235, 12, 255), 0f, Vector2.Zero, 1f, SpriteEffects.None, 1);
 
             spriteBatch.DrawString(Global.gDefaultLargeFont, "Audio", new Vector2(897, 127), Color.FromNonPremultiplied(206, 235, 12, 255), 0f, Vector2.Zero, 1f, SpriteEffects.None, 1);
-
+            spriteBatch.DrawString(Global.gDefaultMediumFont, "Music", new Vector2(806, 227), Color.FromNonPremultiplied(206, 235, 12, 255), 0f, Vector2.Zero, 1f, SpriteEffects.None, 1);
+            spriteBatch.DrawString(Global.gDefaultMediumFont, "Sound", new Vector2(808, 302), Color.FromNonPremultiplied(206, 235, 12, 255), 0f, Vector2.Zero, 1f, SpriteEffects.None, 1);
+            musicDown.Draw(gameTime, param);
+            musicUp.Draw(gameTime, param);
         }
     }
 }
